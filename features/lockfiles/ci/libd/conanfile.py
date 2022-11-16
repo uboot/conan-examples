6cf243b31c0cade50d0c22c9db7a509107317fd4
Matthias Fuchs <matz.fuchs@gmx.at>
2022-11-16 20:53:22 +0100

    Add name to libd

--- features/lockfiles/ci/libd/conanfile.py
+++ features/lockfiles/ci/libd/conanfile.py
@@ -4,5 +4,7 @@ from conans import ConanFile
 required_conan_version = ">=1.28"
 
 class LibDConan(ConanFile):
+    name = "libd"
+    version = "0.1"
     settings = "build_type"
     requires = "libb/[>0.0 <1.0]@user/testing", "libc/[>0.0  <1.0]@user/testing"
